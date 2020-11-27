fab19478007dd3fab9202e50e040354e65408a1a
AntonJanto <anton.janto@gmail.com>
2020-11-27 14:34:16 +0100

    added get method for addres in person class

--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,12 @@ public class Person
 {
   private String name;
   private Address address;
+
+  public Address getAddress()
+  {
+    return address;
+  }
+
   public String getName()
   {
     return name;
