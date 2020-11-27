0888d36ff886d15899ee4f619795f69a1fac172b
AntonJanto <anton.janto@gmail.com>
2020-11-27 13:25:17 +0100

    I made a constructor for car class

--- src/Car.java
+++ src/Car.java
@@ -3,5 +3,9 @@ public class Car
      private String make;
      private String model;
 
-     
+     public Car(String make, String model)
+     {
+          this.make = make;
+          this.model = model;
+     }
 }
