fcbcf2a1a202a15df68c782f5881cae40cb8db27
AntonJanto <anton.janto@gmail.com>
2020-12-02 08:50:27 +0100

    address constructor

--- src/Address.java
+++ src/Address.java
@@ -2,4 +2,10 @@ public class Address
 {
      private String streetName;
      private String zipCode;
+
+     public Address(String streetName, String zipCode)
+     {
+          this.streetName = streetName;
+          this.zipCode = zipCode;
+     }
 }
